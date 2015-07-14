4b483b748929e12b28aef16ff8e2b87194559a4d
mananpreets-optimus <mananpreetsingh.chhatwal@optimusinfo.com>
2015-07-14 17:45:58 +0530

    Assignment1

--- Threading/Assignment/Question1/Resource.java
+++ Threading/Assignment/Question1/Resource.java
@@ -12,15 +12,16 @@ public class Resource {
 	 * it is a synchronized method which calls 
 	 * another synchronized method pen 
 	 */
-	public synchronized int pencil() {
-		return 2*pen();
+	public synchronized int pencil() throws InterruptedException {
+		Thread.sleep(1000);
+		return 2;
 	}
 	/*
 	 * Method Name: pen
 	 * it is a synchronized method which calls
 	 * another synchronized method pencil
 	 */
-	public synchronized int pen() {
+	public synchronized int pen() throws InterruptedException  {
 		return 3*pencil();
 	}
 }
