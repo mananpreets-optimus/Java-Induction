ff14b7f6fecfd26cfd1bbe69e3ef0c8d38796344
mananpreets-optimus <mananpreetsingh.chhatwal@optimusinfo.com>
2015-07-15 13:54:20 +0530

    Collection implementations simple examples

--- Collection/ArrayListExample.java
+++ Collection/ArrayListExample.java
@@ -1,8 +1,14 @@
+/*
+ * Package Name: concepts
+ */
 package concepts;
 
 import java.util.ArrayList;
 import java.util.Iterator;
-
+/*
+ * Class Name: ArrayListExample
+ * involves simple ArrayList.
+ */
 public class ArrayListExample {
 	
 	public static void main(String args[]) {
@@ -12,10 +18,18 @@ public class ArrayListExample {
 		arrayLst.add("Java");
 		arrayLst.add("Development");
 		Iterator<String> itr = arrayLst.iterator();
+		/*
+		 * Iteration of arrayList
+		 * using Iterator instance.
+		 */
 		while (itr.hasNext()) {
 			
 			System.out.println(itr.next());
 		}
+		/*
+		 * Iteration of arrayList
+		 * using for each loop.
+		 */
 		for (String obj : arrayLst) {
 			System.out.println(obj);
 		}
