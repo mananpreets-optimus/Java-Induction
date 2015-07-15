14008338760a2a8dfe41c2f7564fcf5816a4596d
mananpreets-optimus <mananpreetsingh.chhatwal@optimusinfo.com>
2015-07-15 18:33:22 +0530

    Collection Assignment

--- Collection/Assignment/Employee.java
+++ Collection/Assignment/Employee.java
@@ -11,7 +11,7 @@ import java.util.HashMap;
 import java.util.Map;
 /*
  * Class Name: Employee
- * involves setHash
+ * involves setHash.
  */
 public class Employee {
 	
--- Collection/Assignment/EnumDepartment.java
+++ Collection/Assignment/EnumDepartment.java
@@ -1,5 +1,10 @@
+/*
+ *Package Name: assignment
+ */
 package assignment;
-
+/*
+ *Enum Name: EnumDepartment
+ */
 public enum EnumDepartment {
 	IT, OPERATIONS, SALES
 }
